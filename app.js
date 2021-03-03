9515f9a66f5df03ddb62f89c12e78e2c6e9c8b9d
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-03 09:10:33 +0100

    Add AC operation

--- app.js
+++ app.js
@@ -3,31 +3,47 @@ const miniDisplay = document.querySelector('#miniDisplay')
 const display = document.querySelector('#display')
 const numBtns = document.querySelectorAll('.number')
 const opBtns = document.querySelectorAll('.operation')
+const opAc = document.querySelector('#opAc')
+const opEqual = document.querySelector('#opEqual')
+
+let currentValue = 0;
+let currentOperation = '+';
 
 numBtns.forEach(button => button.addEventListener('click', handleNumBtnClick))
 
 function handleNumBtnClick (e) {
-  display.textContent += e.target.value;
+  // display.textContent += e.target.value;
   miniDisplay.textContent += `${e.target.value}`
+  display.textContent = mathMagic(currentOperation, currentValue, parseInt(e.target.value));
 }
 
 opBtns.forEach(button => button.addEventListener('click', handleOpBtnClick))
 
 function handleOpBtnClick (e) {
   miniDisplay.textContent += ` ${e.target.value} `
-  const operator = e.target;
-  const num1 = parseInt(display.textContent)
+  currentOperation = e.target.value
 }
 
 function mathOMatic (e) {
 
 }
 
-const mathMagic = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
-  '/': (a, b) => a / b,
-  '%': a => a / 100,
-  '+/-': a => -a,
+opAc.addEventListener('click', reset)
+
+function reset () {
+  currentValue = 0;
+  currentOperation = '+';
+  display.textContent = currentValue;
+  miniDisplay.textContent = "";
+}
+
+const mathMagic = (operation, num1, num2) => {
+  switch (operation) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
+    case '/': return num1 / num2;
+    case '%': return a / 100;
+    case '+/-': return -a;
+  }
 }
