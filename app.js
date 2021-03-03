c097e7afdd33fcdb7c0d940c4c41fe7b460ab01e
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-03 09:53:24 +0100

    Refactor updateDisplays

--- app.js
+++ app.js
@@ -5,43 +5,57 @@ const numBtns = document.querySelectorAll('.number')
 const opBtns = document.querySelectorAll('.operation')
 const opAc = document.querySelector('#opAc')
 const opEqual = document.querySelector('#opEqual')
+const opPercent = document.querySelector('#opPercent')
 
 let currentValue = 0;
 let currentOperation = '+';
 
 numBtns.forEach(button => button.addEventListener('click', handleNumBtnClick))
 
-function handleNumBtnClick (e) {
-  miniDisplay.textContent += `${e.target.value}`;
+function handleNumBtnClick(e) {
   const newValue = parseInt(e.target.value);
   const result = calculate(currentValue, currentOperation, newValue)
   currentValue = result;
-  display.textContent = newValue;
+  updateDisplays(newValue);
 }
 
 opBtns.forEach(button => button.addEventListener('click', handleOpBtnClick))
 
-function handleOpBtnClick (e) {
-  miniDisplay.textContent += ` ${e.target.value} `
+function handleOpBtnClick(e) {
   currentOperation = e.target.value
-  resolve();
+  updateDisplays(currentValue, e.target.value);
 }
 
 opAc.addEventListener('click', reset);
 
-function reset () {
+function reset() {
   currentValue = 0;
   currentOperation = '+';
   display.textContent = currentValue;
   miniDisplay.textContent = "";
 }
 
-opEqual.addEventListener('click', resolve);
+opEqual.addEventListener('click', () => {
+  updateDisplays(currentValue);
+})
 
-function resolve () {
-  display.textContent = currentValue;
+opPercent.addEventListener('click', () => {
+  currentValue /= 100;
+  updateDisplays(currentValue);
+})
+
+function updateDisplay(value) {
+  display.textContent = value;
 }
 
+function updateMiniDisplay(value) {
+  miniDisplay.textContent += ` ${value} `;
+}
+
+function updateDisplays(displayValue, miniDisplayValue = displayValue) {
+  updateDisplay(displayValue);
+  updateMiniDisplay(miniDisplayValue);
+}
 
 function calculate(num1, operation, num2) {
   switch (operation) {
